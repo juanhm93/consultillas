63ba239405033b35b5c07a9b10a5569ffa5da66b
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2021-04-30 18:13:06 -0400

    base

---
+++ main.js
@@ -0,0 +1,11 @@
+const data = [
+  { categoria: "dev", nivel: 1 },
+  { categoria: "design", nivel: 3 },
+  { categoria: "design", nivel: 7 },
+  { categoria: "dev", nivel: 9 },
+  { categoria: "dev", nivel: 5 },
+  { categoria: "design", nivel: 6 },
+  { categoria: "dev", nivel: 3 },
+];
+
+console.log(data);
